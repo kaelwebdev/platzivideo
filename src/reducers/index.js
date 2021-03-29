4dde8f03a1b2607bd4afcbe70fa16ce698f0a0db
kaelwebdev <cdcp-1795@hotmail.com>
2021-03-29 13:52:37 -0500

    using actionTypes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,15 @@
+import { SET_FAVORITE, DELETE_FAVORITE } from '../actions/actionTypes';
+
 const reducer = (state, action) => {
   switch (action.type) {
     default:
       return state;
-    case 'SET_FAVORITE':
+    case SET_FAVORITE:
       return {
         ...state,
         myList: [...state.myList, action.payload],
       };
-    case 'DELETE_FAVORITE':
+    case DELETE_FAVORITE:
       return {
         ...state,
         myList: state.myList.filter((item) => item.id !== action.payload),
