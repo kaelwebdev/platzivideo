35fb3ce28041ac05a182d4d48d810a893d433fbb
kaelwebdev <cdcp-1795@hotmail.com>
2021-03-29 07:01:05 -0500

    implementing router links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/imgs/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/icons/user-icon.png';
 
-const Header = () => {
-  return (
-    <header className='header'>
+const Header = () => (
+  <header className='header'>
+    <Link to='/'>
       <img className='header__img' src={logo} alt='Platzi Video' />
-      <div className='header__menu'>
-        <div className='header__menu--profile'>
-          <img src={userIcon} alt='' />
-          <p>Perfil</p>
-        </div>
-        <ul>
-          <li><a href='/'>Cuenta</a></li>
-          <li><a href='/'>Cerrar Sesión</a></li>
-        </ul>
+    </Link>
+    <div className='header__menu'>
+      <div className='header__menu--profile'>
+        <img src={userIcon} alt='' />
+        <p>Perfil</p>
       </div>
-    </header>
-  );
-};
+      <ul>
+        <li><a href='/'>Cuenta</a></li>
+        <li>
+          <Link to='/login'>
+            Iniciar Sesión
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </header>
+);
 
 export default Header;
--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../assets/styles/components/Register.scss';
 
 const Register = () => (
@@ -11,7 +12,9 @@ const Register = () => (
         <input className='input' type='password' placeholder='Contraseña' />
         <button className='button' type='submit'>Registrarme</button>
       </form>
-      <a href='empty'>Iniciar sesión</a>
+      <Link to='login'>
+        Iniciar sesión
+      </Link>
     </section>
   </section>
 );
