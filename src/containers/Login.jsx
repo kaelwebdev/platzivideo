668a1380738713db612214dbb31def1008c61bcb
kaelwebdev <cdcp-1795@hotmail.com>
2021-03-29 04:35:46 -0500

    new Login component with route

---
+++ src/containers/Login.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../assets/styles/components/Login.scss';
+import googleIcon from '../assets/static/icons/google-icon.png';
+import twitterIcon from '../assets/static/icons/twitter-icon.png';
+
+const Login = () => (
+  <section className='login'>
+    <section className='login__container'>
+      <h2>Inicia sesión</h2>
+      <form className='login__container--form'>
+        <input className='input' type='text' placeholder='Correo' />
+        <input className='input' type='password' placeholder='Contraseña' />
+        <button className='button' type='button'>Iniciar sesión</button>
+        <div className='login__container--remember-me'>
+          <label htmlFor='cbox1'>
+            <input type='checkbox' id='cbox1' value='first_checkbox' />
+            Recuérdame
+          </label>
+          <a href='/'>Olvidé mi contraseña</a>
+        </div>
+      </form>
+      <section className='login__container--social-media'>
+        <div>
+          <img src={googleIcon} alt='btn login with google' />
+          Inicia sesión con Google
+        </div>
+        <div>
+          <img src={twitterIcon} alt='btn login wit twittet' />
+          Inicia sesión con Twitter
+        </div>
+      </section>
+      <p className='login__container--register'>
+        No tienes ninguna cuenta
+        <a href='empty'>Regístrate</a>
+      </p>
+    </section>
+  </section>
+);
+
+export default Login;
--- webpack.config.js
+++ webpack.config.js
@@ -57,6 +57,7 @@ module.exports = {
     }),
   ],
   devServer: {
+    historyApiFallback: true,
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 3000,
